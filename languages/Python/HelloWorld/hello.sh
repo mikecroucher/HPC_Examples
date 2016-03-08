cb49bd8980d62778f8b5da15ac9a4796d6573d87
Mike Croucher <Michael.P.Croucher@googlemail.com>
2016-03-08 08:14:59 +0000

    HelloWorld.py Submission Script correction

--- languages/Python/HelloWorld/hello.sh
+++ languages/Python/HelloWorld/hello.sh
@@ -4,8 +4,6 @@
 
 #Load the Anaconda Python 3 Environment modulue
 module load apps/python/anaconda3-2.5.0
-#Activate the Python 3 conda environment
-source activate python3
 
 #Run the hello.py program
 python hello.py
