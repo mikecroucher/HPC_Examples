78e877c6228b68b611f1e718c3163018a22cf4a0
M Croucher <sa_fe1mpc@sharc-node008.shef.ac.uk>
2016-11-01 17:55:12 +0000

    Hello World in R and a ShARC submission script

---
+++ languages/R/hello_world/sharc_submit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#$ -cwd                                # Run job from current directory
+#$ -l rmem=4G                          # Request 4 gigabytes of memory
+
+module load apps/R/3.3.2/gcc-4.8.5     # Recommended to load a specific version of R
+
+R CMD BATCH hello.r hello.r.o$JOB_ID
